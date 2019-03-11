58a953fe62d3c84ec43bbc234bb0a158b039479d
gueudaxe <axelgueud@gmail.com>
2019-03-11 18:02:41 +0100

    Ajout alt img

--- js/app.js
+++ js/app.js
@@ -7,31 +7,40 @@ var app = new Vue({
         description: "Game assets / Game Interface / Illustration",
         currentNumber: 0,
         images: [{
-            src: 'assets/images/digiworks/game_assets_1.jpg'
+            src: 'assets/images/digiworks/game_assets_1.jpg',
+            alt: 'Modules végétaux en isométrie'
           },
           {
-            src: 'assets/images/digiworks/game_assets_2.jpg'
+            src: 'assets/images/digiworks/game_assets_2.jpg',
+            alt: 'Modules en isométrie'
           },
           {
-            src: 'assets/images/digiworks/game_assets_3.jpg'
+            src: 'assets/images/digiworks/game_assets_3.jpg',
+            alt: 'Modules en isométrie'
           },
           {
-            src: 'assets/images/digiworks/game_assets_4.jpg'
+            src: 'assets/images/digiworks/game_assets_4.jpg',
+            alt: 'Modules en isométrie'
           },
           {
-            src: 'assets/images/digiworks/game_assets_5.jpg'
+            src: 'assets/images/digiworks/game_assets_5.jpg',
+            alt: 'Modules en isométrie'
           },
           {
-            src: 'assets/images/digiworks/interface_1.jpg'
+            src: 'assets/images/digiworks/interface_1.jpg',
+            alt: 'Interface Menu IWA'
           },
           {
-            src: 'assets/images/digiworks/interface_2.jpg'
+            src: 'assets/images/digiworks/interface_2.jpg',
+            alt: 'Interface in game IWA - simulation'
           },
           {
-            src: 'assets/images/digiworks/illu_1.jpg'
+            src: 'assets/images/digiworks/illu_1.jpg',
+            alt: 'Illustration High Five'
           },
           {
-            src: 'assets/images/digiworks/illu_2.jpg'
+            src: 'assets/images/digiworks/illu_2.jpg',
+            alt: '2 Illustrations isométrie - Concertation & Smartphone '
           }
         ]
       },
@@ -40,31 +49,40 @@ var app = new Vue({
       description: "Illustration / Logo / Pattern",
       currentNumber: 0,
       images: [{
-          src: 'assets/images/devoteam/catcher.png'
+          src: 'assets/images/devoteam/catcher.png',
+          alt: 'Illustration flat design - Claw Machine'
         },
         {
-          src: 'assets/images/devoteam/workspace.png'
+          src: 'assets/images/devoteam/workspace.png',
+          alt: 'Illustration flat design - Working space'
         },
         {
-          src: 'assets/images/devoteam/phare.png'
+          src: 'assets/images/devoteam/phare.png',
+          alt: 'Illustration flat design - Lighthouse'
         },
         {
-          src: 'assets/images/devoteam/kite.png'
+          src: 'assets/images/devoteam/kite.png',
+          alt: 'Illustration flat design - Kite'
         },
         {
-          src: 'assets/images/devoteam/hive.png'
+          src: 'assets/images/devoteam/hive.png',
+          alt: 'Logo Hive Devoteam Intranet'
         },
         {
-          src: 'assets/images/devoteam/kc.png'
+          src: 'assets/images/devoteam/kc.png',
+          alt: 'Logo Knowledge Communities Devoteam'
         },
         {
-          src: 'assets/images/devoteam/cubes.png'
+          src: 'assets/images/devoteam/cubes.png',
+          alt: 'Pattern flat design - Cubes'
         },
         {
-          src: 'assets/images/devoteam/column.png'
+          src: 'assets/images/devoteam/column.png',
+          alt: 'Pattern flat design - Columns'
         },
         {
-          src: 'assets/images/devoteam/lines.png'
+          src: 'assets/images/devoteam/lines.png',
+          alt: 'Pattern flat design - Lines'
         }
       ]
     },
@@ -73,19 +91,24 @@ var app = new Vue({
         description: "Student project - Made with <a href='https://bance-sarah.github.io/portfolio/' target='_blank'>Sarah Bance</a>",
         currentNumber: 0,
         images: [{
-            src: 'assets/images/bfm/logo.jpg'
+            src: 'assets/images/bfm/logo.jpg',
+            alt: 'Logo principal BFMTV rebrand'
           },
           {
-            src: 'assets/images/bfm/logos.jpg'
+            src: 'assets/images/bfm/logos.jpg',
+            alt: 'Déclinaisons logo BFMTV'
           },
           {
-            src: 'assets/images/bfm/cards.jpg'
+            src: 'assets/images/bfm/cards.jpg',
+            alt: 'Mockup cartes visites BFMTV'
           },
           {
-            src: 'assets/images/bfm/wall_1.jpg'
+            src: 'assets/images/bfm/wall_1.jpg',
+            alt: 'Mockup locaux BFMTV'
           },
           {
-            src: 'assets/images/bfm/wall_2.jpg'
+            src: 'assets/images/bfm/wall_2.jpg',
+            alt: 'Mockup locaux ascenseur BFMTV'
           }
         ]
       },
@@ -94,19 +117,24 @@ var app = new Vue({
         description: "Student project - Made with <a href='https://www.behance.net/Mrntrmz' target='_blank'>Marine Termoz</a>",
         currentNumber: 0,
         images: [{
-            src: 'assets/images/prisme/logo.jpg'
+            src: 'assets/images/prisme/logo.jpg',
+            alt: 'Logo principal Prisme branding'
           },
           {
-            src: 'assets/images/prisme/logo_multi.jpg'
+            src: 'assets/images/prisme/logo_multi.jpg',
+            alt: 'Déclinaison logo Prisme'
           },
           {
-            src: 'assets/images/prisme/carte.jpg'
+            src: 'assets/images/prisme/carte.jpg',
+            alt: 'Mockup cartes visites Prisme'
           },
           {
-            src: 'assets/images/prisme/presentation.jpg'
+            src: 'assets/images/prisme/presentation.jpg',
+            alt: 'Mockup impression métallisée Prisme'
           },
           {
-            src: 'assets/images/prisme/vinyle.jpg'
+            src: 'assets/images/prisme/vinyle.jpg',
+            alt: 'Mockup Vinyle Logo'
           }
         ]
       },
@@ -115,25 +143,32 @@ var app = new Vue({
         description: "Various personnal creations",
         currentNumber: 0,
         images: [{
-            src: 'assets/images/random/atlas_m.jpg'
+            src: 'assets/images/random/atlas_m.jpg',
+            alt: 'Photomanipulation Altas portant la Terre'
           },
           {
-            src: 'assets/images/random/holoback.jpg'
+            src: 'assets/images/random/holoback.jpg',
+            alt: 'Affiche Holoback'
           },
           {
-            src: 'assets/images/random/les_pluriels.jpg'
+            src: 'assets/images/random/les_pluriels.jpg',
+            alt: 'Affiche Festival Les Pluriels 20e Edition'
           },
           {
-            src: 'assets/images/random/multimedia_manifesto_m.jpg'
+            src: 'assets/images/random/multimedia_manifesto_m.jpg',
+            alt: 'Affiche DUT MMI'
           },
           {
-            src: 'assets/images/random/psyche.png'
+            src: 'assets/images/random/psyche.png',
+            alt: 'Photomanipulation psychédélique'
           },
           {
-            src: 'assets/images/random/pub.jpg'
+            src: 'assets/images/random/pub.jpg',
+            alt: 'Pub LG TV'
           },
           {
-            src: 'assets/images/random/spoof_ad.jpg'
+            src: 'assets/images/random/spoof_ad.jpg',
+            alt: 'Fake pub Nike'
           }
         ]
       },
