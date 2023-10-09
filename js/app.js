8466c87c16a956529a59c852ef2babd22b03db8c
Axel GUEUDIN <axelgueud@gmail.com>
2023-10-09 17:32:01 +0200

    2023 Update

--- js/app.js
+++ js/app.js
@@ -1,62 +1,87 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    projects: [
+// app.js
+document.addEventListener("DOMContentLoaded", function () {
+  const projects = [
+    {
+      title: "Assassin's Creed Odyssey - Inventory Menu redesign",
+      link: "work/ac-odyssey.html",
+      tag: "UI Design, 2020",
+      description: "Personal project",
+      images: [
+        {
+          src: 'assets/images/ac_ody/cover.png',
+          alt: 'Project cover'
+        }
+      ]
+    },
+    {
+      title: "Project GAIA",
+      link: "work/project-gaia.html",
+      tag: "UI Design, 2020",
+      description: "[WIP] - Personal project",
+      images: [
+        {
+          src: 'assets/images/ui_exo/loadout.png',
+          alt: 'Loadout Menu'
+        }
+      ]
+    },
+    {
+      title: "Master DGDI",
+      link: "work/dgdi.html",
+      tag: "Graphic Design, 2019",
+      description: "Personal project",
+      images: [
+        {
+          src: 'assets/images/dgdi/cover-logos_lq.gif',
+          alt: 'Project cover'
+        }
+      ]
+    },
+    {
+      title: "The Crew - Roster screens redesign",
+      link: "work/the-crew.html",
+      tag: "UI Design, 2020",
+      description: "Personal project",
+      images: [
+        {
+          src: 'assets/images/ubi_crew/cover.png',
+          alt: 'Project cover'
+        }
+      ]
+    },
       {
-        title: "Assassin's Creed Odyssey - Inventory Menu redesign",
-        link: "ac-odyssey.html",
-        tag: "UI Design",
-        description: "Personal project",
-        images: [{
-            src: 'assets/images/ac_ody/cover.png',
-            alt: 'Project cover'
-          }]
-      },
-      {
-        title: "Project GAIA",
-        link: "project-gaia.html",
-        tag: "UI Design",
-        description: "[WIP] - Personal project",
-        images: [{
-            src: 'assets/images/ui_exo/loadout.jpg',
-            alt: 'Loadout Menu'
-          }]
-      },
-      {
-        title: "<a href='/biotica' class='underline'>Biotica</a> - Hybrid game about biodiversity ",
-        tag: "UI Design / Art Direction / Unity Dev",
-        link: "biotica.html",
-        description: "[WIP] - Made with <a href='https://www.behance.net/sarahbance/' class='underline' target='_blank' rel='noreferrer, noopener'>Sarah Bance</a>",
-        images: [{
-            src: 'assets/images/biotica/visuel.jpg',
-            info: 'Visuel de présentation du jeu avec logo',
-            alt: 'Visuel / logo'
-          }]
-      },
-      {
-        title: "Master DGDI",
-        tag: "Branding",
-        link: "dgdi.html",
-        description: "Personal project",
-        images: [{
-            src: 'assets/images/dgdi/cover-behance.jpg',
-            alt: 'Logo DGDI'
-          }]
-      },
-      {
-        title: "Musée de la Chasse & de la Nature",
-        tag: "Branding",
-        link: "mcn.html",
-        description: "Made with <a href='https://www.behance.net/sarahbance/' class='underline' target='_blank' rel='noreferrer, noopener'>Sarah Bance</a>",
-        images: [{
-            src: 'assets/images/mcn/cover.jpg',
-            alt: 'Cover - MCN'
-          }]
-      },
-    ]
-  }
-});
+      title: "Biotica - An eco-friendly board game",
+      link: "work/biotica.html",
+      tag: "Game Design, Art Direction, UI Design, Unity, 2019",
+      description: "with <a href='https://www.behance.net/sarahbance/' class='underline' target='_blank' rel='noreferrer, noopener'>Sarah Bance</a>",
+      images: [
+        {
+          src: 'assets/images/biotica/visuel_lq.png',
+          alt: 'Social media visual'
+        }
+      ]
+    },
+  ];
 
-/* THANKS TO MATHIAS HOEG WEBSITE, FOR THE WORKING SLIDER :)
-tried AJAX + JSON // didnt work well
-Matthias Hager slider tutorial, but had no idea how to loop well the slider*/
+  const projectContainer = document.querySelector(".project-container");
+
+  projects.forEach((project) => {
+    const projectElement = document.createElement("div");
+    projectElement.classList.add('project', 'col', 's12', 'm12', 'l12', 'mb-2');
+
+    projectElement.innerHTML = `
+<div class="description col s12 l2 m2 bt">
+      <h3 class="project-title">${project.title}</h3>
+      <p class="tag">${project.tag}</p>
+      <p class="details">${project.description}</p>
+</div>
+<div class="slider col s12 m9 l9">
+      <a href="${project.link}">
+        <img src="${project.images[0].src}" alt="${project.images[0].alt}">
+      </a>
+</div>
+    `;
+
+    projectContainer.appendChild(projectElement);
+  });
+});
